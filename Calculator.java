ac9092d9717da1e9c9c69e6a2958972bff3c3304
akhilakotha123 <76148924+akhilakotha123@users.noreply.github.com>
2020-12-17 13:39:38 +0530

    first commit
    Calculator class and add().

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+class Calculator{
+  public static int add(int a,int b)
+  {
+    return a+b;
+  }
+
+  public static void main(String[] args)
+  {
+      int a=20,b=10;
+      System.out.println(add(a,b));
+  }
+}
